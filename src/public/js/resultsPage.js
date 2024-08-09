a708d35d9530bffec39d3620935b9ea883ceaa7a
Asmar <Asmar@email.com>
2024-08-09 22:00:04 +0300

    feat(Picker): sort by score

--- src/public/js/resultsPage.js
+++ src/public/js/resultsPage.js
@@ -7,9 +7,11 @@
   const picker = await getPickerDataResponse.json();
 
   const resultsTable = document.getElementById('resultsTable');
-  picker.things.forEach(({ name, score }, idx) => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `<td>${idx + 1}</td><td>${name}</td><td>${score}</td>`;
-    resultsTable.appendChild(tr);
-  });
+  picker.things
+    .sort((a, b) => a.score - b.score)
+    .forEach(({ name, score }, idx) => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `<td>${idx + 1}</td><td>${name}</td><td>${score}</td>`;
+      resultsTable.appendChild(tr);
+    });
 })();
