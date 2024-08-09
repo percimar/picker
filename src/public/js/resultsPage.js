5ee47ca250bda0a12f88f96c0efed89cf940e283
Asmar <Asmar@email.com>
2024-08-09 21:54:38 +0300

    feat(Picker): move into folders, create results page

---
+++ src/public/js/resultsPage.js
@@ -0,0 +1,15 @@
+(async function onload() {
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const pickerId = urlSearchParams.get('pickerId');
+
+  // Get Picker Data
+  const getPickerDataResponse = await getPickerData(pickerId);
+  const picker = await getPickerDataResponse.json();
+
+  const resultsTable = document.getElementById('resultsTable');
+  picker.things.forEach(({ name, score }, idx) => {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `<td>${idx + 1}</td><td>${name}</td><td>${score}</td>`;
+    resultsTable.appendChild(tr);
+  });
+})();
