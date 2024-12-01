cc22dd1e205d2b59634bc9a1d9d17587cb40d21a
Asmar <Asmar@email.com>
2024-12-01 08:22:00 +0300

    feat: setup vite+react frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,8 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), checker({ typescript: { buildMode: true } })],
+});
